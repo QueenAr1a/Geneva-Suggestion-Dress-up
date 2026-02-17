c0307b36427f479b17f47521b65014d03b1d0bf8
Dorian <dorianmuhametaj0920@gmail.com>
2026-02-16 19:24:43 -0500

    Added a slider to countdown to round end

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,18 +5,23 @@ using TMPro;
 using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
+    public Slider slide; 
     public TextMeshProUGUI timerTime;
     private float t;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        t = 60; 
+        t = 60;
+        slide.maxValue = 60;
+        slide.value = 60; 
+        slide.minValue = 0; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        t -= 1 * Time.deltaTime; 
+        t -= 1 * Time.deltaTime;
+        slide.value = t; 
         timerTime.text = t.ToString("0.00");
 
         if(t <= 0.1f)
